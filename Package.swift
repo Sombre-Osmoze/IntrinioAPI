0698bccc3d504bb2f77e7f1fbb4b594c2f8971a7
Marcus Florentin <fmarcus28@gmx.fr>
2019-11-05 04:13:01 +0100

    Package.swift 	• Plateforme support 		• TV OS 13

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "IntrinioAPI",
-	platforms: [.iOS(.v12), .macOS(.v10_14), .watchOS(.v5)],
+	platforms: [.iOS(.v12), .macOS(.v10_14), .watchOS(.v5), .tvOS(.v13)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
