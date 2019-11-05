fdd87129f53e6c292b5520bce8616fd7015531c8
Marcus Florentin <fmarcus28@gmx.fr>
2019-11-05 17:09:44 +0100

    Adding 'Timeframe' enum

---
+++ Sources/IntrinioAPI/Timeframe.swift
@@ -0,0 +1,24 @@
+//
+//  Timeframe.swift
+//  IntrinioAPI
+//
+//  Created by Marcus Florentin on 05/11/2019.
+//
+
+import Foundation
+
+public enum Timeframe: Codable {
+	case m1 = "m1"
+	case m5 = "m5"
+	case m15 = "m15"
+	case m30 = "m30"
+	case H1 = "H1"
+	case H2 = "H2"
+	case H3 = "H3"
+	case H4 = "H4"
+	case H6 = "H6"
+	case H8 = "H8"
+	case D1 = "D1"
+	case W1 = "W1"
+	case M1 = "M1"
+}
